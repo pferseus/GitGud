7ec57920c92d63eeadaf5fb3f6b63772810b61a0
Ame <rainafterd4rk@gmail.com>
2025-11-23 13:32:30 +0800

    Add JLine

--- src/main/java/org/gitgud/Main.java
+++ src/main/java/org/gitgud/Main.java
@@ -1,17 +1,30 @@
 package org.gitgud;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        try {
+            // Create a terminal
+            Terminal terminal = TerminalBuilder.builder()
+                    .system(true)
+                    .build();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+            // Create a line reader
+            LineReader reader = LineReaderBuilder.builder()
+                    .terminal(terminal)
+                    .build();
+
+            // Prompt and read input
+            String line = reader.readLine("JLine > ");
+
+            // Print the result
+            System.out.println("You entered: " + line);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
