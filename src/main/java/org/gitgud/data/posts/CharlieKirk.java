14c9890547ca97a7ea70db463657f3a94a7be577
Leon <galileon.destura@gmail.com>
2025-11-27 17:06:28 +0800

    WE ARE CHARLIE KIRK

---
+++ src/main/java/org/gitgud/data/posts/CharlieKirk.java
@@ -0,0 +1,41 @@
+package org.gitgud.data.posts;
+
+import java.util.List;
+
+import org.gitgud.core.model.AnimatedPost;
+
+@SuppressWarnings("ALL")
+public class CharlieKirk extends AnimatedPost {
+    public CharlieKirk() {
+        super(List.of("""
+                
+                
+                
+                """, """
+                
+                
+                WE
+                """, """
+                
+                WE
+                 CARRY
+                """, """
+                WE
+                 CARRY
+                  THE
+                """, """
+                CARRY
+                  THE
+                   FLAME
+                """, """
+                THE
+                  FLAME
+                    
+                """, """
+                FLAME
+
+
+                """
+        ), 0.1124);
+    }
+}
---
+++ src/main/java/org/gitgud/data/posts/WEARECHARLIEKIRK.java
@@ -0,0 +1,12 @@
+package org.gitgud.data.posts;
+
+import org.gitgud.core.model.StaticPost;
+
+@SuppressWarnings("unused")
+public class WEARECHARLIEKIRK extends StaticPost {
+    public WEARECHARLIEKIRK() {
+        super("""
+                WE ARE CHARLIE KIRK
+                """, true);
+    }
+}
