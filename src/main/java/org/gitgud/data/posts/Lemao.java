2dfd9d2ed3919a844b96e880c6375ae94ff417ed
LemaoSquared <tagleallan321@gmail.com>
2025-11-27 10:07:21 +0000

    confessed to css pres <33333

---
+++ src/main/java/org/gitgud/data/posts/Lemao.java
@@ -0,0 +1,13 @@
+package org.gitgud.data.posts;
+
+import org.gitgud.core.model.StaticPost;
+
+@SuppressWarnings("unused")
+
+public class Lemao extends StaticPost {
+    public Lemao() {
+        super("""
+                LEON PANGET
+                """, true);
+    }
+}
